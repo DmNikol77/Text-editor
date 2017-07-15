dd5c532787cc19bfa6ab3c5a42670659f8809797
DmNikol77 <dm_1992@mail.ru>
2017-07-15 21:17:52 +0300

    Read_Write operations done!

---
+++ TextEditor/TextEditor.BL/FileManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TextEditor.BL
+{
+    public class FileManager
+    {
+        private readonly Encoding defaultEncoding = Encoding.GetEncoding(1251);
+
+        public bool IsExist(string filePath)
+        {
+            bool isExist = File.Exists(filePath);
+            return isExist;
+        }
+
+        public string GetContent(string filePath, Encoding encoding)
+        {
+            string content = File.ReadAllText(filePath, encoding);
+            return content;
+        }
+
+        public string GetContent(string filePath)
+        {
+            return GetContent(filePath, defaultEncoding);
+        }
+
+        public void SaveContent(string filePath, string content, Encoding encoding)
+        {
+            File.WriteAllText(filePath, content, encoding);
+        }
+
+        public void SaveContent(string filePath, string content)
+        {
+            SaveContent(filePath, content, defaultEncoding);
+        }
+
+        public int GetSymbolCount(string content)
+        {
+            int count = content.Length;
+            return count;
+        }
+    }
+}
